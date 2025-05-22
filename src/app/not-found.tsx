598433b4a532f468186adc89284017516f72ac5a
Aniket-Subudh1 <aniketsubudhi00@gmail.com>
2025-05-22 19:37:55 +0530

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { generateMetadata } from "@/utils";
 import { ArrowLeftIcon } from "lucide-react";
 import Link from 'next/link';
-
+import Image from 'next/image';
 export const metadata = generateMetadata({
     title: "404",
     description: "The page you're looking for doesn't exist or has been moved.",
@@ -17,6 +17,16 @@ const NotFoundPage = () => {
             <Wrapper>
                 <Container className="flex flex-col items-center justify-center mx-auto py-16">
                     <div className="flex items-center justify-center h-full flex-col">
+                        <Image 
+                            src="/kk.png"
+                            alt="logo"
+                            className="h-16 w-auto "
+                            width={64}
+                            height={64}
+                        />
+                        <h1 className="text-xl mb-10 md:text-xl font-bold mt-5">
+                            SAAS DSW
+                         </h1>
                         <span className="text-sm px-3.5 py-1 rounded-md bg-gradient-to-br from-sky-400 to-blue-600 text-neutral-50 custom-shadow">
                             404
                         </span>
